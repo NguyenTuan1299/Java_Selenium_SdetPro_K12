d77df97bc0fc9533530c636e5503115f1ee5743d
Tuan <anhtuantn0000@gmail.com>
2023-10-04 00:15:33 +0700

    Commit code for lab4

--- src/java_lab02/Lab2_3.java
+++ src/java_lab02/Lab2_3.java
@@ -16,30 +16,22 @@ public class Lab2_3 {
 
         double minNormalBMI = 18.6;
         double maxNormalBMI = 24.9;
+        double minNormalWeight = minNormalBMI * userHeight * 2;
+        double maxNormalWeight = maxNormalBMI * userHeight * 2;
 
 
         System.out.println("Your BMI is: " + userBMI);
 
         if (userBMI <= 18.5) {
             System.out.println("You are underweight!");
-        } else if (18.5 < userBMI && userBMI < 24.9) {
+            System.out.println("You should increase " + (Math.round(minNormalWeight) - userWeight) + " to " + (Math.round(maxNormalWeight) - userWeight) + " kg!");
+        } else if (userBMI < 24.9) {
             System.out.println("You are normal weight!");
-        } else if (25 < userBMI && userBMI < 29.9) {
+        } else if (userBMI < 29.9) {
             System.out.println("You are overweight!");
+            System.out.println("You should decrease " + (userWeight - Math.round(maxNormalWeight)) + " to " + (userWeight - Math.round(minNormalWeight)) + " kg!");
         } else {
             System.out.println("You are obesity!");
-        };
-
-        if (userBMI <= 18.5) {
-            double minNormalWeight = minNormalBMI * userHeight * 2;
-            double maxNormalWeight = maxNormalBMI * userHeight * 2;
-
-            System.out.println("You should increase " + (Math.round(minNormalWeight) - userWeight) + " to " + (Math.round(maxNormalWeight) - userWeight) + " kg!");
-
-        } else if (25 < userBMI) {
-            double minNormalWeight = minNormalBMI * userHeight * 2;
-            double maxNormalWeight = maxNormalBMI * userHeight * 2;
-
             System.out.println("You should decrease " + (userWeight - Math.round(maxNormalWeight)) + " to " + (userWeight - Math.round(minNormalWeight)) + " kg!");
         }
 
---
+++ src/java_lab04/Lab4.java
@@ -0,0 +1,100 @@
+package java_lab04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.security.SecureRandom;
+
+public class Lab4 {
+    static int ARRAY_LENGTH = 5;
+    static int MAX_VALUE = 1000;
+    static boolean isContinue = true;
+
+    public static void main(String[] args) {
+        List<Integer> myArray = generateRandomArray();
+        printGameMenu();
+        Scanner scanner = new Scanner(System.in);
+        while (isContinue) {
+            System.out.println("Please choose an option in GAME MENU");
+            int userOption = scanner.nextInt();
+            switch (userOption) {
+                case 1:
+                    printAllValue(myArray);
+                    break;
+                case 2:
+                    printMaxValue(myArray);
+                    break;
+                case 3:
+                    printMinValue(myArray);
+                    break;
+                case 4:
+                    searchNum(myArray);
+                    break;
+                case 5:
+                    isContinue = false;
+                    System.out.println("End Game!");
+                    break;
+                default:
+                    System.out.println("PLease enter a number in Menu!");
+            }
+        }
+    }
+
+    private static void printGameMenu() {
+        System.out.println("==== GAME MENU ====");
+        System.out.println("1. Print all numbers");
+        System.out.println("2. Print maximum value");
+        System.out.println("3. Print minimum value");
+        System.out.println("4. Search number");
+        System.out.println("5. Exit");
+    }
+
+    private static List<Integer> generateRandomArray() {
+        List<Integer> randomArray = new ArrayList<>();
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            randomArray.add(new SecureRandom().nextInt(MAX_VALUE));
+        }
+        return randomArray;
+    }
+
+    private static void printAllValue(List<Integer> arrayList) {
+        System.out.print("Your array is: ");
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            System.out.print(arrayList.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printMaxValue(List<Integer> arrayList) {
+        int maxValue = arrayList.get(0);
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (maxValue < arrayList.get(i)) {
+                maxValue = arrayList.get(i);
+            }
+        }
+        System.out.println("The max value is: " + maxValue);
+    }
+
+    private static void printMinValue(List<Integer> arrayList) {
+        int minValue = arrayList.get(0);
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (minValue > arrayList.get(i)) {
+                minValue = arrayList.get(i);
+            }
+        }
+        System.out.println("The min value is: " + minValue);
+    }
+
+    private static String searchNum(List<Integer> arrayList) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input the number you want to search");
+        int inputNum = scanner.nextInt();
+        int indexOfNum = -1;
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (inputNum == arrayList.get(i)) {
+                indexOfNum = i;
+            }
+        }
+        return ((indexOfNum >= 0) ? ("Index of your number is:" + indexOfNum):("Your number is not in the list"));
+    }
+}
